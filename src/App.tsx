e35b661fd0e7bafbfea1de2f9a03b63cd2ebc556
ziedlassoued <zied.lassoued@outlook.de>
2021-11-18 16:34:39 +0100

    :sparkles: Call API from client

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './App.module.css';
 import { Button } from './stories/Button';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then((response) => response.json())
+      .then((result) => setMessage(result.message));
+  }, []);
   return (
     <div className={styles.app}>
       <p>Hello Vite + React!</p>
@@ -14,6 +21,7 @@ function App() {
           label={`count is: ${count}`}
         />
       </p>
+      <p>{message}</p>
     </div>
   );
 }
